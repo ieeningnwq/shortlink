d0a33638b3bc3ea150b3692b3b31163f9c4dae7d
niewening <nie_wq@163.com>
2024-06-29 18:42:09 +0800

    增加用户查询脱敏和不脱敏信息

--- admin/src/main/java/com/ieening/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/ieening/shortlink/admin/controller/UserController.java
@@ -4,9 +4,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ieening.shortlink.admin.common.convention.result.Result;
 import com.ieening.shortlink.admin.common.convention.result.Results;
+import com.ieening.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.ieening.shortlink.admin.dto.resp.UserRespDTO;
 import com.ieening.shortlink.admin.service.UserService;
 
+import cn.hutool.core.bean.BeanUtil;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,10 +23,18 @@ public class UserController {
     private final UserService userService;
 
     /**
-     * 根据用户名查询用户信息
+     * 根据用户名查询脱敏后用户信息
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByName(@PathVariable("username") String username) {
         return Results.success(userService.getUserByUsername(username));
+    }
+
+    /**
+     * 根据用户名查询用户真实信息
+     */
+    @GetMapping("/api/shortlink/v1/actual/user/{username}")
+    public Result<UserActualRespDTO> getActualUserByName(@PathVariable("username") String username) {
+        return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
     }
 }
